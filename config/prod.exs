d6c1905e14dc98444af6259cd07766c4e29ed2f0
katdon <katdon@st.amu.edu.pl>
2018-12-02 21:42:16 +0100

    Try again prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,12 +10,18 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :slackir, SlackirWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
-  #http: [:inet6, port: System.get_env("PORT") || 4000],
+  http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
 force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Configure your database
+config :slackir, Slackir.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -72,11 +78,6 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
 
-# Configure your database
-config :slackir, Slackir.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+
 
 
